c7f417b7ea6cf8732f44c6ebed2d936c359b6962
Jacob Jeffries <88559006+jwjeffr@users.noreply.github.com>
2025-11-19 14:11:52 -0500

    ovito script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ovito-3d-object
 
 web page with a 3d object from ovito
+
+hosted [here](https://jwjeffr.github.io/ovito-3d-object/)
---
+++ main.py
@@ -0,0 +1,39 @@
+from tempfile import TemporaryDirectory
+
+from pymatgen.core import Structure
+from ovito.io import import_file, export_file
+from ovito.modifiers import CreateBondsModifier, ClusterAnalysisModifier, ExpressionSelectionModifier, DeleteSelectedModifier
+
+
+def main():
+
+    with TemporaryDirectory() as tmpdir:
+
+        structure = Structure.from_file("petn.cif")
+        structure.to(fmt="POSCAR", filename=f"{tmpdir}/POSCAR")
+        pipeline = import_file(f"{tmpdir}/POSCAR")
+        pipeline.modifiers.append(
+            CreateBondsModifier(mode=CreateBondsModifier.Mode.VdWRadius)
+        )
+        pipeline.modifiers.append(
+            ClusterAnalysisModifier(unwrap_particles=True, neighbor_mode=ClusterAnalysisModifier.NeighborMode.Bonding)
+        )
+        pipeline.modifiers.append(
+            ExpressionSelectionModifier(expression="Cluster!=2")
+        )
+        pipeline.modifiers.append(
+            DeleteSelectedModifier()
+        )
+        pipeline.add_to_scene()
+        particles_vis_element = pipeline.compute().particles.vis
+        particles_vis_element.scaling = 0.5
+
+        cell_vis_element = pipeline.compute().cell.vis
+        cell_vis_element.render_cell = False
+
+        export_file(None, file="config.glb", format="gltf")
+
+
+if __name__ == "__main__":
+
+    main()
